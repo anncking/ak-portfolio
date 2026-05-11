483bf52f5496e86e19ea350f2d313809a03a5b73
anncking <anncking75@gmail.com>
2026-05-11 14:40:15 -0700

    Implement dropdown menu functionality
    Added functionality to toggle dropdown menu visibility and close it when clicking outside.

--- scripts.js
+++ scripts.js
@@ -28,3 +28,23 @@ let slideIndex = 1;
             // Display the current slide (adjusted for zero-based array index)
             slides[slideIndex - 1].style.display = "block";
         }
+/* Menu drop down */
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
